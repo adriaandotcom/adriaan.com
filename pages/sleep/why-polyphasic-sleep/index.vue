e47c0f42a17599fd9fbab374c2f81bb8bee23f85
Adriaan van Rossum <1079135+adriaanvanrossum@users.noreply.github.com>
2024-02-06 20:33:08 +0100

    Explain examples

--- pages/sleep/why-polyphasic-sleep/index.vue
+++ pages/sleep/why-polyphasic-sleep/index.vue
@@ -11,7 +11,7 @@
       Boosting productivity is a constant challenge for me. As a developer, the
       difference between being fully productive and just somewhat productive is
       like comparing racing on a bicycle to moving along with a kick scooter.
-      That&#39;s how I got interested in polyphasic sleep and going to do my own
+      That's how I got interested in polyphasic sleep and going to do my own
       schedule for one month.
     </p>
 
@@ -26,9 +26,9 @@
         target="_blank"
         >πολύς</a
       >
-      (polús), meaning &quot;many&quot; or &quot;much.&quot; This isn&#39;t new
+      (polús), meaning &quot;many&quot; or &quot;much.&quot; This isn't new
       information for most. When it comes to polyphasic sleep, you might infer
-      it involves multiple phases of sleep. And that&#39;s exactly right.
+      it involves multiple phases of sleep. And that's exactly right.
     </p>
     <p>
       Instead of sleeping for one long stretch of hours, it involves sleeping in
@@ -39,34 +39,65 @@
 
     <Phases class="mt-6 md:-ml-[10%] md:w-[120%] stroke-[2.3px] max-w-none" />
 
+    <h4>Siesta</h4>
     <p>
-      There are enough studies about sleep, and if you&#39;re into that, you
-      might want to listen to the podcast of Matthew Walker, the author of the
-      book Why we sleep. He shows multiple studies suggesting that polyphasic
-      sleep doesn&#39;t offer the promised benefits. Instead, it often leads to
-      disrupted sleep stages, impaired cognitive function, mood decline, and
-      adverse effects on health, including blood sugar and cardiovascular
-      issues.
+      It's a classic in sunny Spain and beyond, where you snooze for 5-6 hours
+      at night, then top it off with a 20-minute to 1-hour nap when the sun's at
+      its peak. It's the perfect excuse to avoid meetings post-lunch and blame
+      it on cultural heritage.
     </p>
+
+    <h4>Monophasic</h4>
     <p>
-      Great, so health wise it&#39;s not a smart move. Although, I think, that
+      Monophasic sleep is the vanilla ice cream of sleep patterns—classic,
+      reliable, but a bit plain. You dive into bed for a 7-9 hour sleep marathon
+      every night. It's what you most likely do.
+    </p>
+
+    <h4>Everyman</h4>
+    <p>
+      Enter the Everyman schedule, where you try to outsmart nature by breaking
+      your sleep into a main 3-4 hour chunk and several mini "coffee break"
+      naps. You'll end up with about 4 hours of sleep per day.
+    </p>
+
+    <h4>Uberman</h4>
+    <p>
+      You can see that the uberman (not related to Andrew Huberman) is super
+      extreme. It has 6 phases of 20 minutes totalling to 2 hours per day. You
+      can see where this goes...
+    </p>
+
+    <h3 id="health-issues">Health issues</h3>
+    <p>
+      There are enough studies about sleep, and if you're into that, you might
+      want to listen to the podcast of Matthew Walker, the author of the book
+      Why we sleep. He shows multiple studies suggesting that polyphasic sleep
+      doesn't offer the promised benefits. Instead, it often leads to disrupted
+      sleep stages, impaired cognitive function, mood decline, and adverse
+      effects on health, including blood sugar and cardiovascular issues.
+    </p>
+
+    <p>
+      Great, so health wise it's not a smart move. Although, I think, that
       mostly links to the many phases most polyphasic sleeper schedule and in
-      total they&#39;er usually a lot shorter than the 8 hours that we all seem
-      to need. My try will be getting those 8 hours of sleep at least while
-      trying out polyphasic sleep.
+      total they'er usually a lot shorter than the 8 hours that we all seem to
+      need. My try will be getting those 8 hours of sleep at least while trying
+      out polyphasic sleep.
     </p>
+
     <h3 id="productive-hours">Productive hours</h3>
     <p>
-      One thing I really don&#39;t like is only getting productive later in the
-      day. I often begin my day at a coffee shop, working without distractions.
-      After lunch, I usually experience a slump in energy. I do get a bit of
-      higher productivity back by late afternoon, but the afternoon is normally
-      good enough for lighter tasks. Then, it&#39;s time for social activities,
-      like dinner or hanging out with friends. If I start working on my laptop
-      around 9pm, I lose track of time and can work until 2am. However, this
-      pattern messes up the next day due to lack of sleep and possibly poorer
-      sleep quality, as my brain is still buzzing right before bed. Clearly,
-      this routine isn&#39;t effective.
+      One thing I really don't like is only getting productive later in the day.
+      I often begin my day at a coffee shop, working without distractions. After
+      lunch, I usually experience a slump in energy. I do get a bit of higher
+      productivity back by late afternoon, but the afternoon is normally good
+      enough for lighter tasks. Then, it's time for social activities, like
+      dinner or hanging out with friends. If I start working on my laptop around
+      9pm, I lose track of time and can work until 2am. However, this pattern
+      messes up the next day due to lack of sleep and possibly poorer sleep
+      quality, as my brain is still buzzing right before bed. Clearly, this
+      routine isn't effective.
     </p>
     <p>
       As my productive hours are during the mornings and late in the evening,
@@ -84,55 +115,54 @@
       easy work (calls, meetings, admin, customer support). Then I wind down for
       an hour and sleep from 2pm to 6pm. After waking, I spend time with my
       girlfriend or engage in social activities. Around 9pm, I head back to the
-      office or a coffee shop (found one that&#39;s open until midnight!) for a
-      few 90-minute intense work sessions. Leaving the office at 2am feels
-      almost magical, with the few people out having their own reasons for being
-      awake.
+      office or a coffee shop (found one that's open until midnight!) for a few
+      90-minute intense work sessions. Leaving the office at 2am feels almost
+      magical, with the few people out having their own reasons for being awake.
     </p>
     <h3 id="wife-acceptance-factor">Wife acceptance factor</h3>
     <p>
       The question I received most frequently when I talked about my polyphasic
       sleep plans was, &quot;What does your girlfriend think about this?&quot;
-      Maybe it&#39;s because of all the home automation devices I&#39;ve set up
-      in our apartment, or perhaps she knows that resisting it would only make
-      me more determined to do it, or simply because she&#39;s an incredible
-      woman, but she was almost as excited as I was. &quot;This really suits
-      you,&quot; she told me.
+      Maybe it's because of all the home automation devices I've set up in our
+      apartment, or perhaps she knows that resisting it would only make me more
+      determined to do it, or simply because she's an incredible woman, but she
+      was almost as excited as I was. &quot;This really suits you,&quot; she
+      told me.
     </p>
     <h3 id="what-will-be-difficult-">What will be difficult?</h3>
     <p>
       One challenge for me will be waking up twice. Getting up once a day is
-      hard enough; doing it again sounds like a double dose of trouble. I&#39;m
+      hard enough; doing it again sounds like a double dose of trouble. I'm
       really curious to see how this goes over the month.
     </p>
 
     <Clock class="w-32 md:-mr-10 ml-4 float-right stroke-2 max-w-none" />
 
     <p>
-      I&#39;ve never been good at napping. It just doesn&#39;t work for me—I end
-      up feeling groggier after those 20-30 minutes of trying to sleep. And how
+      I've never been good at napping. It just doesn't work for me—I end up
+      feeling groggier after those 20-30 minutes of trying to sleep. And how
       easy is it to snooze during the day, anyway? Will I be wrestling with my
-      melatonin levels? This stuff tells your body it&#39;s bedtime, peaking at
+      melatonin levels? This stuff tells your body it's bedtime, peaking at
       night and dipping in the morning.
     </p>
     <p>
-      Weekends might get tricky, socially speaking. My girlfriend&#39;s on board
-      with this, but it&#39;s going to be awkward asking to nap at a
-      friend&#39;s or family member&#39;s place. Then again, they probably know
-      me well enough to just laugh it off.
+      Weekends might get tricky, socially speaking. My girlfriend's on board
+      with this, but it's going to be awkward asking to nap at a friend's or
+      family member's place. Then again, they probably know me well enough to
+      just laugh it off.
     </p>
 
     <h3 id="advantages-so-far">Advantages so far</h3>
     <p>
-      Since I have to wind down twice a day now, I&#39;m taking way more walks.
-      Just the walk to the office is about 12 minutes one way, so do that four
-      times and I&#39;m walking nearly an hour every day.
+      Since I have to wind down twice a day now, I'm taking way more walks. Just
+      the walk to the office is about 12 minutes one way, so do that four times
+      and I'm walking nearly an hour every day.
     </p>
     <p>
       When I kicked this off last week, I tried to tackle the heavy work in the
-      morning, but that just wasn&#39;t cutting it. Turns out, those late-night
-      focus sessions are when I really get down to business, and that&#39;s
-      what&#39;s working best for me.
+      morning, but that just wasn't cutting it. Turns out, those late-night
+      focus sessions are when I really get down to business, and that's what's
+      working best for me.
     </p>
   </div>
 </template>
